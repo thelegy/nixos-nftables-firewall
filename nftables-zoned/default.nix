3580061262d57b9d4e3d49c25dd4f9543ec77325
Jan Beinke <git@janbeinke.com>
2021-01-27 15:59:15 +0100

    Move nft firewall helper functions to another file

--- nftables-zoned/default.nix
+++ nftables-zoned/default.nix
@@ -1,5 +1,6 @@
-{ config, lib, ... }:
+args@{ config, lib, ... }:
 with lib;
+with import ./common_helpers.nix args;
 
 let
 
@@ -136,35 +137,6 @@ in {
 
       zoneFwdIngressChainName = zone: "nixos-firewall-forward-${zone.name}-ingress";
       zoneFwdTraversalChainName = ingressZone: egressZone: "nixos-firewall-forward-${ingressZone.name}-to-${egressZone.name}";
-
-      renderChains = rawChains: chainNames: let
-        processJumpRule = {onExpression?"", jump}: let
-          chain = chains."${jump}";
-          topLine = if length chain < 1 then "" else head chain;
-          topLineContents = topLine.line or topLine;
-          topLineDeps = topLine.chainDeps or [];
-          canInline = length chain < 1 || (length chain < 2 && ! hasInfix ";" topLineContents);
-          what = if canInline then topLineContents else "jump ${jump}";
-          line = if onExpression != "" then "${onExpression} ${what}" else what;
-          chainDeps = if canInline then topLineDeps else unique (topLineDeps ++ [jump]);
-          newRule = if chainDeps == [] then line else {inherit line chainDeps;};
-        in if length chain < 1 then "" else newRule;
-        processRule = rule: if rule ? jump then processJumpRule rule else rule;
-        chains = mapAttrs (_: flip pipe [
-          singleton
-          flatten
-          (map (x: if isString x then splitString "\n" x else x))
-          flatten
-          (map processRule)
-          (filter (x: x!=""))
-        ]) rawChains;
-        renderChain = chainName: ''
-          chain ${chainName} {${
-            concatMapStrings (line: "\n  ${line.line or line}") (chains.${chainName} or [])}
-          }
-        '';
-        findDeps = names: if length names < 1 then [] else names ++ findDeps (flatten (map (name: concatMap (l: l.chainDeps or []) chains."${name}") names));
-      in pipe chainNames [ findDeps  unique (concatMapStrings renderChain) ];
 
       chains = {
 
